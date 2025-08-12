0412d6861ac6b6ba2440e6ed3b1ac4d3a1b1b0b1
TaufeeqAhmad123 <130512752+TaufeeqAhmad123@users.noreply.github.com>
2025-08-12 15:44:33 +0500

    counter example

--- riverpod/lib/home_screen.dart
+++ riverpod/lib/home_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final hello = Provider((ref) {
-  return 'Hello, Riverpod!';
+final counter = StateProvider<int>((ref) {
+  return 0;
+});
+final switchProvider = StateProvider<bool>((ref) {
+  return false;
 });
 
 class HomeScreen extends ConsumerWidget {
@@ -10,10 +13,50 @@ class HomeScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final greeting = ref.watch(hello);
+    print('build');
     return Scaffold(
       appBar: AppBar(title: const Text('Home Screen')),
-      body: Center(child: Text(greeting)),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Consumer(
+            //we use conumer so that only the counter widget build not the entire widget state
+            builder: (context, ref, child) {
+              final count = ref.watch(counter);
+              print('Counter build');
+              return Column(
+                children: [
+                  Text('Counter: $count', style: TextStyle(fontSize: 24)),
+                ],
+              );
+            },
+          ),
+          Consumer(
+            builder: (context, ref, child) {
+              final switchValue = ref.watch(switchProvider);
+              print('Switch build');
+              return Switch(
+                value: switchValue,
+                onChanged: (value) {
+                  ref.read((switchProvider.notifier)).state = value;
+                },
+              );
+            },
+          ),
+          ElevatedButton(
+            onPressed: () {
+              ref.read(counter.notifier).state++;
+            },
+            child: Icon(Icons.add),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              ref.read(counter.notifier).state--;
+            },
+            child: Icon(Icons.remove),
+          ),
+        ],
+      ),
     );
   }
 }
