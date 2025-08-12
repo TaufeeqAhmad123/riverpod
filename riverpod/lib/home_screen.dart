5a0f445e061c5111b7de6941f2e936faf261ed97
TaufeeqAhmad123 <130512752+TaufeeqAhmad123@users.noreply.github.com>
2025-08-12 15:23:05 +0500

    first

--- riverpod/lib/home_screen.dart
+++ riverpod/lib/home_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeScreen extends StatelessWidget {
+final hello = Provider((ref) {
+  return 'Hello, Riverpod!';
+});
+
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return  Scaffold();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final greeting = ref.watch(hello);
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Screen')),
+      body: Center(child: Text(greeting)),
+    );
   }
 }
